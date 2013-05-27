0f9bcdc1f656c19f41c8b451400a2fd734afd20d
felipe bastos <felipeabm@gmail.com>
2013-05-27 08:30:43 -0300

    Update Welcome.php

--- classes/Controller/Admin/Welcome.php
+++ classes/Controller/Admin/Welcome.php
@@ -6,6 +6,9 @@ class Controller_Admin_Welcome extends Controller {
     {
         $template = new View_Jade('layout/admin');
         $view = new View_Jade('admin/welcome/index');
+        $view->products = ORM::factory('Product')->find_all();
+        $view->categories = ORM::factory('Category')->find_all();
+        $view->orders = ORM::factory('Order')->find_all();
         $template->content = $view;
         $this->response->body($template);
     }
