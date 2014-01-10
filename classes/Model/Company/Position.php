e15fe990945b4efcafbf92501cdbee4afbaa3f0d
felipe bastos <felipeabm@gmail.com>
2014-01-10 17:15:43 -0200

    Update Position.php

--- classes/Model/Company/Position.php
+++ classes/Model/Company/Position.php
@@ -2,6 +2,19 @@
 
 class Model_Company_Position extends ORM {
 
-
+    public $_table_name = "company_position";
+    public $_table_name_plural = false;
+    public $_primary_key = "id";
+    
+    public $_belongs_to = array();
+    
+    public $_has_one = array();
+    
+    public $_has_many = array(
+        "functions" => array(
+            "model" => "Company_Function",
+            "foreign_key" => "position_id"
+        ),
+    );
 
 }
