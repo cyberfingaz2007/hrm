27945b85ec2db5464d0f4c3ab320c657b7268bdf
felipe bastos <felipeabm@gmail.com>
2014-01-10 17:17:18 -0200

    Update Function.php

--- classes/Model/Company/Function.php
+++ classes/Model/Company/Function.php
@@ -5,5 +5,16 @@ class Model_Company_Function extends ORM {
     public $_table_name = "company_function";
     public $_table_name_plural = false;
     public $_primary_key = "id";
+    
+    public $_belongs_to = array(
+        "position" => array(
+            "model" => "Company_Position",
+            "foreign_key" => "position_id"
+        ),
+    );
+    
+    public $_has_one = array();
+    
+    public $_has_many = array();
 
 }
