8d74404fe2a1fd2a12b675c99744b9d303514845
felipe bastos <felipeabm@gmail.com>
2014-01-22 21:53:28 -0200

    Create employee.php

---
+++ classes/model/employee.php
@@ -0,0 +1,12 @@
+<?php
+
+class Model_Employee extends ORM {
+
+	/**
+    * @var Long $id not null autoincrement primary key
+    * @var String $name not null
+    * @var String $sex not null
+    * @var Date $birthday not null
+    */
+
+}
