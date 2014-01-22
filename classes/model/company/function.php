6978a7bad18f5c8339673f8748be14d44237d899
felipe bastos <felipeabm@gmail.com>
2014-01-22 21:36:22 -0200

    Create function.php

---
+++ classes/model/company/function.php
@@ -0,0 +1,24 @@
+<?php
+
+class Model_Company_Function extends ORM {
+
+    /**
+    * @var Long $id not null autoincrement primary key
+    * @var String $name not null
+    * @var String $description not null
+    */
+
+	// Table description
+    public $_table_name = 'company_function';
+    public $_table_name_plural = false;
+    public $_primary_key = 'id';
+    
+    // Table relations
+    
+    public $_belongs_to = array();
+    
+    public $_has_one = array();
+    
+    public $_has_many = array();
+
+}
