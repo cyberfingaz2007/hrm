52e3be6a5ec1e8366a1ca66fdcc86b4b8a51fb58
felipe bastos <felipeabm@gmail.com>
2014-01-25 23:04:10 -0200

    Create company.php

---
+++ classes/controller/hrm/company.php
@@ -0,0 +1,43 @@
+<?php
+
+class Controller_Hrm_Company extends Controller_Template {
+
+	public $template = 'default/layout/main';
+	
+	public $user_logged;
+	
+	public function before()
+	{
+		parent::before();
+		
+		$this->user_logged = Filter_Security::userLogged();
+	}
+	
+	public function action_index()
+	{
+		// TODO: list companies User is employeeds
+		$companies = ORM::factory('company')->find_all();
+		
+		$view = View::factory('default/hrm/company/index');
+		$view->companies = $companies;
+		$this->template->content = $view;
+	}
+	
+	public function action_create()
+	{
+		$company = ORM::factory('company');
+		
+		if($this->request->post())
+		{
+			$company->name = $this->request->post('name');
+			$company->save();
+		}
+		
+		
+		
+		$view = View::factory('default/hrm/company/create');
+		$view->company = $company;
+		$this->template->content = $view;
+	}
+
+}
