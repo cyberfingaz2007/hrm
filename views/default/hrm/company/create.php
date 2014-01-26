fda4594964a79b73039d421643ce52b628e8b34a
felipe bastos <felipeabm@gmail.com>
2014-01-26 00:27:20 -0200

    Update create.php

--- views/default/hrm/company/create.php
+++ views/default/hrm/company/create.php
@@ -1,6 +1,6 @@
 <div class="container">
 	<div class="row">
-		<div class="span12">
+		<div class="col-md-12">
 			<?php echo Form::open('hrm/company/create'); ?>
 				<h1>Create Company</h1>
 				<label><?php echo __('Name'); ?>:</label>
