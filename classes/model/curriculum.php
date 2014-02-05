ae8d4ad3dd15327155f6d10b1b116ab0975a01bf
felipe bastos <felipeabm@gmail.com>
2014-02-05 16:04:19 -0200

    Create curriculum.php

---
+++ classes/model/curriculum.php
@@ -0,0 +1,18 @@
+<?php
+
+class Model_Curriculum extends ORM {
+
+	/**
+	 * @var Long $id not null autoincrement primary key
+	 * @var String $name not null
+	 */
+
+	public $_table_name = '';
+	public $_table_name_plural = false;
+	public $_primary_key = 'id';
+
+	public $_belongs_to = array();
+	public $_has_one = array();
+	public $_has_many = array();
+
+}
