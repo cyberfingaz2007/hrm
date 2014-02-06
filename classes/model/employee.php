710e63be115f2d715f976d109662eda8a83a3c47
felipe bastos <felipeabm@gmail.com>
2014-02-06 12:43:14 -0200

    Update employee.php

--- classes/model/employee.php
+++ classes/model/employee.php
@@ -1,12 +1,42 @@
 <?php
 
-class Model_Employee extends ORM {
+class Model_Employee extends People {
 
 	/**
     * @var Long $id not null autoincrement primary key
-    * @var String $name not null
-    * @var String $sex not null
-    * @var Date $birthday not null
+    * @var Salary $salary not null
+    * @var Company $company_id not null
+    * @var Company_Position $company_position_id not null
+    * @var Company_Function $company_function_id not null
+    * @var DateTime $date_create not null
+    * @var DateTime $date_admission not null
+    * @var DateTime $date_demission not null
     */
+    
+    // Table description
+    public $_table_name = 'people';
+    public $_table_name_plural = false;
+    public $_primary_key = 'id';
+    
+    // Table relations
+    
+    public $_belongs_to = array(
+    	'people' => array(
+    		'model' => 'people',
+    		'foreign_key' => 'people_id'
+    	),
+    	'company_position' => array(
+    		'model' => 'company_position',
+    		'foreign_key' => 'company_position_id'
+    	),
+    	'company_function' => array(
+    		'model' => 'company_function',
+    		'foreign_key' => 'company_function_id'
+    	),
+    );
+    
+    public $_has_one = array();
+    
+    public $_has_many = array();
 
 }
