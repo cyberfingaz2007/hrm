001d9b7c560e9cf5354c8d092bc2739ab4cd89b0
felipe bastos <felipeabm@gmail.com>
2014-02-07 19:14:14 -0200

    Update degree.php

--- classes/model/curriculum/degree.php
+++ classes/model/curriculum/degree.php
@@ -6,5 +6,21 @@ class Model_Curriculum_Deegre extends ORM {
 	 * @var Long $id not null autoincrement primary key
 	 * @var String $name not null
 	 */
+	 
+	 // Table description
+	 public $_table_name = 'curriculum_degree';
+	 public $_table_name_plural = false;
+	 public $_primary_key = 'id';
+	 
+	 public $_belongs_to = array();
+	 
+	 public $_has_one = array();
+	 
+	 public $_has_many = array(
+	 	'curriculums' => array(
+	 		'model' => 'curriculum',
+	 		'foreign_key' => 'curriculum_degree_id'
+	 	),
+	 );
 
 }
