cf34e664bd35244fe298d57ff78a7db52dbbad4e
felipe bastos <felipeabm@gmail.com>
2014-02-09 00:52:56 -0200

    Update create.php

--- views/default/hrm/company/create.php
+++ views/default/hrm/company/create.php
@@ -3,20 +3,7 @@
 		<div class="col-md-12">
 			<?php echo Form::open('hrm/company/create'); ?>
 				<h1>Create Company</h1>
-				<label><?php echo __('Name'); ?>:</label>
-				<input type="text" name="name" />
-				<label><?php echo __('Address'); ?>:</label>
-				<input type="text" name="address['street']" />
-				<label><?php echo __('Number'); ?>:</label>
-				<input type="text" name="address['number']" />
-				<label><?php echo __('City'); ?>:</label>
-				<input type="text" name="address['city']" />
-				<label><?php echo __('State'); ?>:</label>
-				<input type="text" name="address['state']" />
-				<label><?php echo __('Zipcode'); ?>:</label>
-				<input type="text" name="address['zipcode']" />
-				<label><?php echo __('Country'); ?>:</label>
-				<input type="text" name="address['country']" />
+				<?php include 'form.php'; ?>
 				<br />
 				<input type="submit" name="company_create" value="Create" />
 			<?php echo Form::close(); ?>
